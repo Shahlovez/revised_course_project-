85006b082ee6379f0e083beaf1a8f3e74276f277
Shahlo Ovezniyazova <76026227+Shahlovez@users.noreply.github.com>
2022-01-18 10:39:34 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+        $(function() {
+            $(".carousel").carousel( { interval: 2000 } );
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                    $(".carousel").carousel("pause");
+                    $("#carouselButton").children("i").removeClass("fa-pause");
+                    $("#carouselButton").children("i").addClass("fa-play");
+                } else {
+                    $(".carousel").carousel("cycle");
+                    $("#carouselButton").children("i").removeClass("fa-play");
+                    $("#carouselButton").children("i").addClass("fa-pause"); 
+                }
+            });
+        });
+
+// Task 2
+
+        $(function(){
+            $("#loginButton").click(function() {
+                $("#loginModal").modal("show");
+            });
+        })
+        
+        $(function() {
+            $("#reserveButton").click(function() {
+                $("#reserveModal").modal("show");
+                console.log("test");
+            });
+        })
+
+
+
+
+        
